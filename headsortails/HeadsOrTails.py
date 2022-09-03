16fa6e952b6cfef1d795cbfb4ac919d07808167d
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 19:10:29 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -1,2 +1,15 @@
 import random
 def heads():
+  return "heads"
+def tails():
+  return "tails"
+ans = input("Heads or Tails?")
+if random.choice([True,False]):
+  coin_side = tails()
+else:
+  coin_side = heads()
+if ans == coin_side():
+  print("Correct")
+else:
+    print("Wrong the correct answer was " + coin_side + " and not " + ans)
+    print("Better luck next time!")
