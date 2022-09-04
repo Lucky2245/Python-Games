1ab9a444643180f1d6b94c62839b6b9525f6612f
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 22:50:53 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -1,31 +1,31 @@
 try:
    import random
    def heads():
-	   return "heads"
+       return "heads"
    def tails():
-	   return "tails"
+	    return "tails"
    ans = input("Heads or Tails? ")
    if random.choice([True, False]):
       coin_side = tails() 
    else:
-	 coin_side = heads()
+	    coin_side = heads()
    if ans == coin_side:
       print("Correct!")
    else:
-	    print("Wrong the correct answer was " + coin_side + " and not " + ans)
-	    print("Better luck next time!")
+	     print("Wrong the correct answer was " + coin_side + " and not " + ans)
+	     print("Better luck next time!")
 except:
+       import random
        def heads():
-         return "heads"
+           return "heads"
        def tails():
-         return "tails"
-       ans = input("Heads or Tails? ")
-       if random.choice([True,False]):
+           return "tails"
+       ans = input("Heads or Tails?")
+       if(random.choice([True,False])):
           coin_side = heads()
        else:
             coin_side = tails()
-            if ans == coin_side:
-               print("Correct!")
-            else:
-                 print("Wrong the correct answer is " + coin_side + " and not " + ans)
-                 print("Better luck next time!")
+       if ans == coin_side:
+          print("Correct!")
+       else:
+            print("Wrong the correct answer was " + coin_side + " and not " + ans)
