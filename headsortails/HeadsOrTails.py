d00fb065a767a132d4dbb781ef2215d02b8de2be
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 23:21:22 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -26,10 +26,12 @@ except:
        else:
             coin_side = tails()
        if ans == coin_side:
-          print("Correct!")
+	print("Correct!")
        else:
 	print("Wrong the correct answer was " + coin_side + " and not " + ans)
         print("Better luck next time")
 	other_games = import("Want to play a different game?")   
 	if other_games == "Yes" or "yes":
-		import sleepingdragons
+		import sleepingdragon
+	else:
+		print("Game Over!")	
