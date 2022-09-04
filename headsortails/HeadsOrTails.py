1d8ecded8d8871a482f96e9a7dd540c141f46fbc
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 23:52:11 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -1,45 +1,21 @@
-try:
-   import random
-   def heads():
-       return "heads"
-   def tails():
-	    return "tails"
-   ans = input("Heads or Tails? ")
-   if random.choice([True, False]):
-      coin_side = tails() 
-   else:
-	    coin_side = heads()
-   if ans == coin_side:
-      print("Correct!")
-   else:
-		print("Wrong the correct answer was " + coin_side + " and not " + ans)
-		print("Better luck next time!")
-		games = input("Want to play a different game? This will be a screen based game ")
-		if games == "Yes" or "yes":
-			import sleepingdragons
-		else:
-			print("Game over!")
-except:
-       import random
-       def heads():
-           return "heads"
-       def tails():
-           return "tails"
-       ans = input("Heads or Tails?")
-       if(random.choice([True,False])):
-          coin_side = heads()
-       else:
-            coin_side = tails()
-       if ans == coin_side:
-	print("Correct!")
-	OTHER_GAMES = input("Want to play a different game? This will be a screen based game ")
-	if OTHER_GAMES == "Yes" or "yes":
-		import sleepingdragons
-       else:
-	print("Wrong the correct answer was " + coin_side + " and not " + ans)
-        print("Better luck next time")
-	other_games = input("Want to play a different game? This will be a screen based game ")
-	if other_games == "Yes" or "yes":
-		import sleepingdragons
-	else:
-		print("Game Over!")	
+import random
+def heads():
+	return "heads"
+def tails():
+	return "tails"
+ans = input("Heads or Tails? ")
+if random.choice([True, False]):
+   coin_side = tails() 
+else:
+	 coin_side = heads()
+if ans == coin_side:
+   print("Correct!")
+   other_games = input("want to play a different game?")
+   if other_games == "Yes" or "yes":
+       import sleepingdragons
+else:
+	 print("Wrong the correct answer was " + coin_side + " and not " + ans)
+	 other_games = input("Want to play a different?")
+	 if other_games == "Yes" or "yes":
+	     import sleepingdragons
+	 print("Better luck next time!")
