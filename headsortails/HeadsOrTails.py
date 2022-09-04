35c99d3d0b3471c9456571948a0acdf1c3319318
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 23:52:58 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -10,12 +10,12 @@ else:
 	 coin_side = heads()
 if ans == coin_side:
    print("Correct!")
-   other_games = input("want to play a different game?")
+   other_games = input("want to play a different game? This game will be a screen-based game")
    if other_games == "Yes" or "yes":
        import sleepingdragons
 else:
 	 print("Wrong the correct answer was " + coin_side + " and not " + ans)
-	 other_games = input("Want to play a different?")
+	 other_games = input("Want to play a different? This game will be a screen-based game")
 	 if other_games == "Yes" or "yes":
 	     import sleepingdragons
 	 print("Better luck next time!")
