b627e695188a0b78e3c8158e240e7d13305a4c47
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-04 00:00:38 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -13,6 +13,10 @@ if ans == coin_side:
    other_games = input("want to play a different game? This game will be a screen-based game")
    if other_games == "Yes" or "yes":
        import sleepingdragons
+   elif other_games == "No" or "no":
+       print("Game Over")
+       print("Game Over")
+       
 else:
 	 print("Wrong the correct answer was " + coin_side + " and not " + ans)
 	 print("Better luck next time!")
