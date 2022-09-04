eeb112fd415a586aab6ecfa3b30cf409a8a8c803
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-03 22:30:32 -0400

    Update HeadsOrTails.py

--- headsortails/HeadsOrTails.py
+++ headsortails/HeadsOrTails.py
@@ -1,19 +1,31 @@
-import random
-def heads():
-	return "heads"
-def tails():
-	return "tails"
-ans = input("Heads or Tails? ")
-if random.choice([True, False]):
-   coin_side = tails() 
-else:
+try:
+   import random
+   def heads():
+	   return "heads"
+   def tails():
+	   return "tails"
+   ans = input("Heads or Tails? ")
+   if random.choice([True, False]):
+      coin_side = tails() 
+   else:
 	 coin_side = heads()
-if ans == coin_side:
-   print("Correct!")
-else:
-	print("Wrong the correct answer was " + coin_side + " and not " + ans)
-	play_new_game = input("Want to play a new game?")
-	if play_new_game == "yes":
-		import game
-	else:
-		print("Game Over")
+   if ans == coin_side:
+      print("Correct!")
+   else:
+	    print("Wrong the correct answer was " + coin_side + " and not " + ans)
+	    print("Better luck next time!")
+except:
+       def heads():
+         return "heads"
+       def tails():
+         return "tails"
+       ans = input("Heads or Tails? ")
+       if random.choice([True,False]):
+          coin_side = heads()
+       else:
+            coin_side = tails()
+            if ans == coin_side:
+               print("Correct!")
+            else:
+                 print("Wrong the correct answer is " + coin_side + " and not " + ans)
+                 print("Better luck next time!")
